b2a7037318b54bdd4096b34d9c3b012ee7f7f52b
이수진 <soojin4964@gmail.com>
2020-04-23 04:20:25 +0000

    Revert "Merge branch 'feature/MLmodel' into 'develop'"
    This reverts merge request !33

--- back/found/models.py
+++ back/found/models.py
@@ -1,9 +1,3 @@
 from django.db import models
-from ..ai.models import Category, Color, Image
 
-
-class Found(models.Model):
-    password = models.CharField(max_length=300)
-    # lost_date = models.
-
-    # TODO django_advanced insta image 참고하고 datetime field 부터 시작하기
+# Create your models here.
